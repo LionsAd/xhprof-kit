a58711bfbedfb5e1f9d18ba8da5ed320a0f747c9
Fabian Franz <github@fabian-franz.de>
2012-12-14 07:31:30 +0100

    Use more reliable calculation

--- find-min-web.sh
+++ find-min-web.sh
@@ -2,7 +2,7 @@
 #
 # Syntax: find-min.sh <namespace> <loops>
 
-MIN=1000000
+MIN=1000000000
 NS='drupal-perf'
 LOOPS=100
 [ -n "$1" ] && NS=$1
@@ -10,12 +10,12 @@ LOOPS=100
 
 for i in $(seq 1 $LOOPS)
 do
-	curl -s 'http://127.0.0.1/index-perf.php?extra='"$NS" | grep 'loop time: |0.'
+	curl -s 'http://127.0.0.1/index-perf.php?extra='"$NS" | grep 'loop time: |'
 	#curl -H "Accept: application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5" -s 'http://127.0.0.1/node/1?extra='"$NS" | grep 'loop time: |0.'
 	#curl -H "Accept: text/html,application/xml,application/xhtml+xml;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5" -s 'http://127.0.0.1/node/1?extra='"$NS" | grep 'loop time: |0.'
 done | while read line
 do
-	mytime=$(echo $line | cut -d'|' -f2 | cut -d'.' -f2 | cut -d's' -f1)
+	mytime=$(echo $line | cut -d'|' -f2 | cut -d's' -f1 | sed 's/^/1000*1000*/' | cut -d'.' -f1 | bc)
 
 	if [ $mytime -lt $MIN ]
 	then
