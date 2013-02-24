378013413d096466567b40a8e131247c782c9e6c
Fabian Franz <github@fabian-franz.de>
2013-02-24 14:57:23 +0100

    Make it possible to use xhprof-kit without devel (its broken anyway)

--- docroot/index-perf.php
+++ docroot/index-perf.php
@@ -3,12 +3,41 @@
 // set profiler namespace 
 $profiler_namespace = (!isset($_GET['namespace']))?'drupal-perf':$_GET['namespace'];
 $profiler_extra = (!isset($_GET['extra']))?'':$_GET['extra'];
+$profiler_dir = 'xhprof-kit/xhprof';
 
-// @todo: This still needs devel module with xhprof enabled
+if (extension_loaded('xhprof')) {
+    include_once $profiler_dir . '/xhprof_lib/utils/xhprof_lib.php';
+    include_once $profiler_dir . '/xhprof_lib/utils/xhprof_runs.php';
+    xhprof_enable(XHPROF_FLAGS_CPU + XHPROF_FLAGS_MEMORY);
+}
 
+// Parse URL
+$benchmark_url = '/';
+if (isset($_GET['url'])) {
+  $benchmark_url = $_GET['url'];
+}
+
+// define default settings
+$cmd = 'index.php';
+
+$path = parse_url($benchmark_url);
+
+if (isset($path['query'])) {
+  $_SERVER['QUERY_STRING'] = $path['query'];
+  parse_str($path['query'], $_GET);
+  $_REQUEST = $_GET;
+}
+
+// set file to execute or Drupal path (clean URLs enabled)
+if (isset($path['path'])) {
+  $_SERVER['SCRIPT_NAME'] = '/' . $cmd;
+  $_SERVER['REQUEST_URI'] = $path['path'];
+}
+
+// Benchmark loop
 $time_start = microtime( true );
 
-include 'index.php';
+include $cmd;
 
 $time_end = ( microtime( true ) - $time_start );
 printf( "loop time: |%fs|",
@@ -21,7 +50,7 @@ if (extension_loaded('xhprof')) {
     $run_id = $xhprof_runs->save_run($xhprof_data, $profiler_namespace);
 
     // url to the XHProf UI libraries (change the host name and path)
-    $profiler_url = sprintf('http://' . $base_url . '/xhprof/xhprof_html/index.php?run=%s&source=%s&extra=%s', $run_id, $profiler_namespace, $profiler_extra);
+    $profiler_url = sprintf($base_url . '/xhprof-kit/xhprof/xhprof_html/index.php?run=%s&source=%s&extra=%s&url=%s', $run_id, $profiler_namespace, $profiler_extra, $benchmark_url);
     echo $run_id . '|' . $profiler_namespace . '|' . $profiler_extra . '|' . '<a href="'. $profiler_url .'" target="_blank">Profiler output</a>' . "\n";
 }
 
