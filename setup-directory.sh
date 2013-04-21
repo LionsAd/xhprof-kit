f1178fc9b31caaa4f106053bede358d66d9dd3f9
Fabian Franz <github@fabian-franz.de>
2013-04-21 17:24:22 +0200

    Add setup script for preparing a drupal directory.

---
+++ setup-directory.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+BASE_DIR=$(dirname $0)
+
+ln -sf "$BASE_DIR" xhprof-kit || { echo "Error: Could not setup xhprof-kit."; exit 1; }
+ln -sf "$BASE_DIR/docroot/index-perf.php" index-perf.php || { echo "Error: Could not setup index-perf.php."; exit 1; }
+echo "XHProf-Kit was setup successfully." 1>&2
