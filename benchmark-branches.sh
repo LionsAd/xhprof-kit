ec93d06ee2be497b3e68cd1c69d715147c7b48ab
Fabian Franz <github@fabian-franz.de>
2013-04-21 17:22:21 +0200

    Make it possible to save data to a file

--- benchmark-branches.sh
+++ benchmark-branches.sh
@@ -5,6 +5,9 @@ b_branch=$2
 shift
 shift
 
+mkdir -p $(dirname $0)/data/
+exec 3>$(dirname $0)"/data/${b_base}-${b_branch}.data"
+
 for branch in $* 
 do
         $(dirname $0)/benchmark-vs-baseline.sh "$branch" $b_base $b_branch
