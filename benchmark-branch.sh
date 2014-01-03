7cb768991761db52584c1985ee35d24755498fb9
Scott Reeves <cottser@mailworks.org>
2014-01-02 22:51:04 -0500

    Update `drush cc all` to `drush cr`
    `drush cc all` is now deprecated and doesn't work for Drupal 8.

--- benchmark-branch.sh
+++ benchmark-branch.sh
@@ -3,7 +3,7 @@
 branch=master
 [ -n "$1" ] && branch=$1
 
-drush cc all 2>/dev/null
+drush cr 2>/dev/null
 git checkout -q "$branch" --
 
 settings_php=settings.default.php
@@ -16,7 +16,7 @@ fi
 #sudo ln -sf "$settings_php" sites/default/settings.php
 
 sudo rm -rf sites/default/files/php
-drush cc all 2>/dev/null
+drush cr 2>/dev/null
 sudo rm -rf sites/default/files/php
 drush rr 2>/dev/null
 sudo rm -rf sites/default/files/php
