aa91285e7aff2e398907f36d516ff22e2ef3cf72
Fabian Franz <github@fabian-franz.de>
2014-10-28 09:29:26 +0000

    Add helper file to copy runs to directory.

---
+++ copy-runs-to-directory.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]
+then
+	echo "Usage: $0 <collector-file> <destination-directory> [namespace]"
+	exit 1
+fi
+
+export RUN_COLLECTOR="$1"
+export DESTINATION="$2"
+export FIND_NAMESPACE="|$3"
+
+XHPROF_DIR=$(php -r 'print ini_get("xhprof.output_dir");')
+
+rm -f "$DESTINATION"/*
+
+export IFS="|"
+cat "$RUN_COLLECTOR" | grep "$FIND_NAMESPACE" | while read run_id namespace
+do
+	cp -f "$XHPROF_DIR/$run_id"* $DESTINATION/"${run_id}.${namespace}.xhprof"
+done
