3c3a96a4f37796792e4555fb8d1654afb522202a
Fabian Franz <github@fabian-franz.de>
2014-10-28 08:03:13 +0000

    Collect runs into a collector file.

--- benchmark-branches.sh
+++ benchmark-branches.sh
@@ -7,10 +7,13 @@ shift
 
 mkdir -p $(dirname $0)/data/
 exec 3>$(dirname $0)"/data/${b_base}-${b_branch}.data"
+export XHPROF_KIT_COLLECTOR_FILE=$(dirname $0)"/data/${b_base}-${b_branch}.runs"
+echo -n > "$XHPROF_KIT_COLLECTOR_FILE"
 
 for branch in $* 
 do
         $(dirname $0)/benchmark-vs-baseline.sh "$branch" $b_base $b_branch
 done
+unset XHPROF_KIT_COLLECTOR_FILE
 echo "---"
 echo "ct = function calls, wt = wall time, cpu = cpu time used, mu = memory usage, pmu = peak memory usage"
--- find-min-web.sh
+++ find-min-web.sh
@@ -6,9 +6,15 @@ MIN=1000000000
 NS='drupal-perf'
 LOOPS=100
 URL='/'
+RUN_COLLECTOR=""
+[ -n "$XHPROF_KIT_COLLECTOR_FILE" ] && RUN_COLLECTOR=$XHPROF_KIT_COLLECTOR_FILE
 [ -n "$1" ] && NS=$1
 [ -n "$2" ] && LOOPS=$2
 [ -n "$3" ] && URL=$3
+[ -n "$4" ] && RUN_COLLECTOR=$4
+
+# Do one call to ensure caches are primed.
+curl -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -s 'http://127.0.0.1/index-perf.php?extra='"$NS"'&url='"$URL" | grep 'loop time: |' &>/dev/null
 
 for i in $(seq 1 $LOOPS)
 do
@@ -20,6 +26,12 @@ do
 	then
 		MIN=$mytime
 		echo $line
+	fi
+	if [ -n "$RUN_COLLECTOR" ]
+	then
+		RUN_ID=$(echo $line | cut -d'|' -f3)
+                SANITIZED_NS=$(echo $NS | sed 's/\./_/g')
+		echo "${RUN_ID}|${SANITIZED_NS}" >> $RUN_COLLECTOR
 	fi
 	LC_ALL=C sleep 0.1
 done
