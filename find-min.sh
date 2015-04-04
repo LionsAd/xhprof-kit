df46f7ca2f4a37901eff13ef4bfb0b188248bdec
Scott Reeves <cottser@mailworks.org>
2015-04-04 17:27:32 -0400

    Allow overriding the docroot with $XHPROF_KIT_DOCROOT

--- find-min.sh
+++ find-min.sh
@@ -5,12 +5,13 @@
 MIN=1000000
 NS='drupal-perf'
 LOOPS=100
+: ${XHPROF_KIT_DOCROOT:="127.0.0.1"}
 [ -n "$1" ] && NS=$1
 [ -n "$2" ] && LOOPS=$2
 
 for i in $(seq 1 $LOOPS)
 do
-	sudo ionice -c1 -n0 nice -n -20 chrt 99 taskset -c 1 ./core/scripts/drupal-perf.sh --namespace "$NS" 'http://127.0.0.1/node' | egrep 'loop time|xhprof'
+	sudo ionice -c1 -n0 nice -n -20 chrt 99 taskset -c 1 ./core/scripts/drupal-perf.sh --namespace "$NS" 'http://'"$XHPROF_KIT_DOCROOT"'/node' | egrep 'loop time|xhprof'
 done | while read line
 do
 	mytime=$(echo $line | cut -d'|' -f2 | cut -d'.' -f2 | cut -d's' -f1)
