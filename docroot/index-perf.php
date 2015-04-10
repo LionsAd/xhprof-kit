644f456c465f726e6404255b56386a1683883af9
Daniel Wehner <daniel@tag1consulting.com>
2015-04-10 17:20:18 +0200

    quick help improvement

--- docroot/index-perf.php
+++ docroot/index-perf.php
@@ -52,6 +52,9 @@ register_shutdown_function(function() use ($time_start, $profiler_namespace, $be
     $profiler_url = sprintf($base_url . '/xhprof-kit/xhprof/xhprof_html/index.php?source=%s&url=%s&run=%s&extra=%s', $profiler_namespace, urlencode($benchmark_url), $run_id, $profiler_extra);
     echo $run_id . '|' . $profiler_namespace . '|' . $profiler_extra . '|' . '<a href="'. $profiler_url .'" target="_blank">Profiler output</a>' . "\n";
   }
+  else {
+    echo "Extension not loaded, maybe restart apache after the installation.";
+  }
 
 });
 
