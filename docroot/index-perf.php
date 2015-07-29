907ba982ec225f7a6aea76418574212c21796985
Fabian Franz <github@fabian-franz.de>
2015-07-29 23:50:47 +0000

    Fix subdirectories and missing base_url, move xhprof loading later.

--- docroot/index-perf.php
+++ docroot/index-perf.php
@@ -5,16 +5,12 @@ $profiler_namespace = (!isset($_GET['namespace']))?'drupal-perf':$_GET['namespac
 $profiler_extra = (!isset($_GET['extra']))?'':$_GET['extra'];
 $profiler_dir = 'xhprof-kit/xhprof';
 
-if (extension_loaded('xhprof')) {
-    include_once $profiler_dir . '/xhprof_lib/utils/xhprof_lib.php';
-    include_once $profiler_dir . '/xhprof_lib/utils/xhprof_runs.php';
-    xhprof_enable(XHPROF_FLAGS_MEMORY);
-}
-
 // Parse URL
 $benchmark_url = '/';
 if (isset($_GET['url'])) {
   $benchmark_url = $_GET['url'];
+  unset($_GET['url']);
+  $_SERVER['QUERY_STRING'] = '';
 }
 
 // define default settings
@@ -30,7 +26,9 @@ if (isset($path['query'])) {
 
 // set file to execute or Drupal path (clean URLs enabled)
 if (isset($path['path'])) {
-  $_SERVER['SCRIPT_NAME'] = '/' . $cmd;
+  $_SERVER['SCRIPT_FILENAME'] = str_replace(basename(__FILE__), $cmd, $_SERVER['SCRIPT_FILENAME']);
+  $_SERVER['SCRIPT_NAME'] = str_replace(basename(__FILE__), $cmd, $_SERVER['SCRIPT_NAME']);
+  $_SERVER['PHP_SELF'] = str_replace(basename(__FILE__), $cmd, $_SERVER['PHP_SELF']);
   $_SERVER['REQUEST_URI'] = $path['path'];
 }
 
@@ -48,6 +46,10 @@ register_shutdown_function(function() use ($time_start, $profiler_namespace, $be
     $xhprof_runs = new XHProfRuns_Default();
     $run_id = $xhprof_runs->save_run($xhprof_data, $profiler_namespace);
 
+if (!isset($base_url)) {
+  $base_url = '';
+}
+
     // url to the XHProf UI libraries (change the host name and path)
     $profiler_url = sprintf($base_url . '/xhprof-kit/xhprof/xhprof_html/index.php?source=%s&url=%s&run=%s&extra=%s', $profiler_namespace, urlencode($benchmark_url), $run_id, $profiler_extra);
     echo $run_id . '|' . $profiler_namespace . '|' . $profiler_extra . '|' . '<a href="'. $profiler_url .'" target="_blank">Profiler output</a>' . "\n";
@@ -55,6 +57,11 @@ register_shutdown_function(function() use ($time_start, $profiler_namespace, $be
 
 });
 
+if (extension_loaded('xhprof')) {
+    include_once $profiler_dir . '/xhprof_lib/utils/xhprof_lib.php';
+    include_once $profiler_dir . '/xhprof_lib/utils/xhprof_runs.php';
+    xhprof_enable(XHPROF_FLAGS_MEMORY);
+}
 
 include $cmd;
 
