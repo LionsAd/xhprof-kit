42a189d86c224cd951f7dd9d5b99e3fb2cc64273
Fabian Franz <github@fabian-franz.de>
2016-06-14 16:01:27 +0000

    Make find-min.sh faster.

--- find-min.sh
+++ find-min.sh
@@ -11,15 +11,14 @@ LOOPS=100
 
 for i in $(seq 1 $LOOPS)
 do
-	sudo ionice -c1 -n0 nice -n -20 chrt 99 taskset -c 1 ./core/scripts/drupal-perf.sh --namespace "$NS" 'http://'"$XHPROF_KIT_DOCROOT"'/node' | egrep 'loop time|xhprof'
+	sudo ionice -c1 -n0 nice -n -20 chrt 99 taskset -c 1 php ./index-perf.php --namespace "$NS" 'http://'"$XHPROF_KIT_DOCROOT"'/node' | egrep 'loop time|xhprof'
 done | while read line
 do
-	mytime=$(echo $line | cut -d'|' -f2 | cut -d'.' -f2 | cut -d's' -f1)
+        mytime=$(echo $line | cut -d'|' -f2 | cut -d's' -f1 | sed 's/^/1000*1000*/' | bc | cut -d'.' -f1)
 
 	if [ $mytime -lt $MIN ]
 	then
 		MIN=$mytime
 		echo $line
 	fi
-	LC_ALL=C sleep 0.1
 done
