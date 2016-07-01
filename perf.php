e20848f7ea99159820085f3c41d20dcb992316fc
Fabian Franz <github@fabian-franz.de>
2016-07-01 13:49:51 +0000

    Add perf helper script.

--- perf.php
+++ perf.php
@@ -1,38 +1,49 @@
 <?php
 
-# To use just include somewhere in the code.
+namespace XhprofKit;
 
-$profiler_namespace = (!isset($_GET['namespace']))?'drupal-perf':$_GET['namespace'];
-$profiler_extra = (!isset($_GET['extra']))?'':$_GET['extra'];
-$profiler_dir = __DIR__ . '/xhprof';
+define('XHPROF_KIT_PROFILER_DIR', __DIR__ . '/xhprof');
+
+# To use just include somewhere in the code.
 
 // enable xhprof by default
 $enable_xhprof = extension_loaded('xhprof');
 
-// Benchmark loop
-$time_start = microtime( true );
+class XhprofKitRun {
 
-register_shutdown_function(function() use ($time_start, $profiler_namespace, $benchmark_url, $profiler_extra, $enable_xhprof) {
-  $time_end = ( microtime( true ) - $time_start );
-  $output = sprintf( "loop time: |%fs|",
-      $time_end
-  );
+  public $namespace = 'drupal-perf';
 
-  if ($enable_xhprof) {
-    $xhprof_data = xhprof_disable();
-    $xhprof_runs = new XHProfRuns_Default();
-    $run_id = $xhprof_runs->save_run($xhprof_data, $profiler_namespace);
-    $base_url = '';
+  public function __construct($namespace = 'drupal-perf') {
+    $this->enableXhprof = extension_loaded('xhprof');
+    $this->timeStart = microtime( true );
+    $this->namespace = $namespace;
+    if ($this->enableXhprof) {
+      include_once XHPROF_KIT_PROFILER_DIR . '/xhprof_lib/utils/xhprof_lib.php';
+      include_once XHPROF_KIT_PROFILER_DIR . '/xhprof_lib/utils/xhprof_runs.php';
+      xhprof_enable(XHPROF_FLAGS_MEMORY);
+    }
+  }
 
-    // Get path for profiler url.
-    $profiler_url = sprintf($base_url . '/xhprof-kit.php/?source=%s&url=%s&run=%s&extra=%s', $profiler_namespace, urlencode($benchmark_url), $run_id, $profiler_extra);
-    $output .= $run_id . '|' . $profiler_namespace . '|' . $profiler_extra . '|' . '<a id="xhprof-profiler-output" href="'. $profiler_url .'" target="_blank">Profiler output</a>' . "\n";
+  static function create($namespace = 'drupal-perf') {
+    return new static($namespace);
+  }
+
+  public function __destruct() {
+    $time_end = ( microtime( true ) - $this->timeStart );
+    $output = sprintf( "loop time: |%fs|",
+        $time_end
+    );
+
+    if ($this->enableXhprof) {
+      $xhprof_data = xhprof_disable();
+      $xhprof_runs = new \XHProfRuns_Default();
+      $run_id = $xhprof_runs->save_run($xhprof_data, $this->namespace);
+      $base_url = '';
+
+      // Get path for profiler url.
+      $profiler_url = sprintf($base_url . '/xhprof-kit.php/?source=%s&url=%s&run=%s', $this->namespace, 'cli', $run_id);
+      $output .= $run_id . '|' . $this->namespace . '|' . $profiler_url . "\n";
+    }
     error_log($output, 3, '/tmp/perf.log');
   }
-});
-
-if ($enable_xhprof) {
-  include_once $profiler_dir . '/xhprof_lib/utils/xhprof_lib.php';
-  include_once $profiler_dir . '/xhprof_lib/utils/xhprof_runs.php';
-  xhprof_enable(XHPROF_FLAGS_MEMORY);
 }
